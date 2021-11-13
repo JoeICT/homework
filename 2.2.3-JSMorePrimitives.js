3dff6471b0730bd32fc869e854e1252cc42226d5
Joseph Hunter <jrhunt1706@gmail.com>
2021-11-13 17:20:36 -0600

    JS Variables and Primitives Part 3

---
+++ 2.2.3-JSMorePrimitives.js
@@ -0,0 +1,44 @@
+
+// create a variable "timeOfDay" and assign it a string
+let timeOfDay = 'morning';
+// create a variable "greeting" that references a template literal
+let greeting = "great";
+// use "timeOfDay" in the template literal to create a message like "Good morning!" or "Good evening!"
+let text = `It is a ${greeting}, ${timeOfDay}!`;
+// print "greeting"
+console.log (text);
+
+// create a new variable, but do not assign it a value
+let x
+// print the variable and its type
+console.log (typeof "x");
+// ? what type should we expect?
+
+// string
+
+// ? what value should we assign?
+
+//undefined
+
+
+// print the variable and its type again
+console.log (typeof "x");
+
+// ? what type should we expect?
+
+// string
+
+// try to print a variable that does not exist
+console.log (C);
+
+// ? what should we expect to print in the CLI?
+
+//undefined
+
+// print "greeting" again
+
+console.log (text);
+
+// ? will this line run?
+
+//no
