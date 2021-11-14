1e0b6a7394e3d47cd9e5a1a4d141e8eff5d4f24a
Joseph Hunter <jrhunt1706@gmail.com>
2021-11-13 21:55:09 -0600

    Pizza Place week 2

---
+++ week2PizzaPlc.js
@@ -0,0 +1,12 @@
+let pizzaPlace = "Joes Pizza";
+let numberOfToppings = 3;
+console.log (pizzaPlace + " " + numberOfToppings);
+console.log(`Welcome to ${pizzaPlace} where you get
+${numberOfToppings} toppings for the price of one.`);
+let offer = 5;
+if ( offer < 10) {
+console.log = "quality not quantity";
+} else {
+console.log = "a whole lot of pizza";
+}
+
