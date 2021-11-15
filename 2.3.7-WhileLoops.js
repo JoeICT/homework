5b4b1eda1310be52c234617cbbeeb78777e519a4
Joseph Hunter <jrhunt1706@gmail.com>
2021-11-15 00:27:26 -0600

    2.3.7 while loops week2

---
+++ 2.3.7-WhileLoops.js
@@ -0,0 +1,16 @@
+// ? TO DO: create a while loop that TOTALS the numbers from 1 to 100
+// create a variable to represent the current number
+let x = 100;
+// create a variable to represent the current total
+let a = 0;
+// write a while loop that sums the numbers from 1 to 100
+
+
+    while (a<100) {
+
+      x += a;
+      a++;
+
+    }
+
+  console.log (x);
